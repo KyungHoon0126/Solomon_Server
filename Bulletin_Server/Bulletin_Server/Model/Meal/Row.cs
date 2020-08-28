94cac73b205f850bc06151e35dcb3b48f00fea2a
김경훈 <kidi052812@gmail.com>
2020-08-28 15:31:14 +0900

    [ADD] : Row Model

--- Bulletin_Server/Bulletin_Server/Model/Meal/Row.cs
+++ Bulletin_Server/Bulletin_Server/Model/Meal/Row.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace Bulletin_Server.Model.Meal
 {
-    class Row
+    public class Row
     {
+        //[JsonProperty("ATPT_OFCDC_SC_CODE")]
+        //public string ATPT_OFCDC_SC_CODE { get; set; }
+
+        //[JsonProperty("ATPT_OFCDC_SC_NM")]
+        //public string ATPT_OFCDC_SC_NM { get; set; }
+
+        //[JsonProperty("SD_SCHUL_CODE")]
+        //public string SD_SCHUL_CODE { get; set; }
+
+        //[JsonProperty("SCHUL_NM")]
+        //public string SCHUL_NM { get; set; }
+
+        //[JsonProperty("MMEAL_SC_CODE")]
+        //public string MMEAL_SC_CODE { get; set; }
+
+        [JsonProperty("MMEAL_SC_NM")]
+        public string MEAL_TYPE { get; set; }
+
+        //[JsonProperty("MLSV_YMD")]
+        //public string MLSV_YMD { get; set; }
+
+        //[JsonProperty("MLSV_FGR")]
+        //public string MLSV_FGR { get; set; }
+
+        [JsonProperty("DDISH_NM")]
+        public string MEAL_MENUS { get; set; }
+
+        //[JsonProperty("ORPLC_INFO")]
+        //public string ORPLC_INFO { get; set; }
+
+        //[JsonProperty("CAL_INFO")]
+        //public string CAL_INFO { get; set; }
+
+        [JsonProperty("NTR_INFO")]
+        public string DETAIL_INFO { get; set; }
+
+        //[JsonProperty("MLSV_FROM_YMD")]
+        //public string MLSV_FROM_YMD { get; set; }
+
+        //[JsonProperty("MLSV_TO_YMD")]
+        //public string MLSV_TO_YMD { get; set; }
     }
 }
