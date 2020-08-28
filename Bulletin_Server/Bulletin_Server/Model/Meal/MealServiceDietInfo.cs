7e78b1c4b679eaf1f7147853973993cd57e30560
김경훈 <kidi052812@gmail.com>
2020-08-28 15:29:52 +0900

    [ADD] : Create MealServiceDietInfo Model

--- Bulletin_Server/Bulletin_Server/Model/Meal/MealServiceDietInfo.cs
+++ Bulletin_Server/Bulletin_Server/Model/Meal/MealServiceDietInfo.cs
@@ -1,14 +1,14 @@
 ﻿using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bulletin_Server.Model.Meal
 {
     public class MealServiceDietInfo
     {
-        
+        //[JsonProperty("head")]
+        //public List<Head> Head { get; set; }
+
+        [JsonProperty("row")]
+        public List<Row> row { get; set; }
     }
 }
