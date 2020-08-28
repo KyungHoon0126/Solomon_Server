22a773e1cb83393eaba8db1ac0ec2903a0542c43
김경훈 <kidi052812@gmail.com>
2020-08-28 15:26:56 +0900

    [ADD] : Create MealServiceDietInfo

---
+++ Bulletin_Server/Bulletin_Server/Model/Meal/MealServiceDietInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulletin_Server.Model.Meal
+{
+    public class MealServiceDietInfo
+    {
+        [JsonProperty("list_total_count", NullValueHandling = NullValueHandling.Ignore)]
+        public long? ListTotalCount { get; set; }
+        [JsonProperty("RESULT", NullValueHandling = NullValueHandling.Ignore)]
+        public Result Result { get; set; }
+    }
+}
