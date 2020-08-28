17f2764323179fb028bce2d4ed7a869f5e204f45
김경훈 <kidi052812@gmail.com>
2020-08-28 15:48:40 +0900

    [ADD] : Program.cs Server Side

--- Bulletin_Server/Bulletin_Server/Program.cs
+++ Bulletin_Server/Bulletin_Server/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bulletin_Server
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
+            var server = new WebServiceHost(typeof(Service.BulletinService));
+            server.AddServiceEndpoint(typeof(IService), new WebHttpBinding(), "");
+            server.Open();
+            Console.WriteLine("Bulletin Server Start");
+            Console.WriteLine("If you want to exit this application, please push enter key.");
+            Console.ReadLine();
+            Console.WriteLine("Bulletin Server Stop");
         }
     }
 }
