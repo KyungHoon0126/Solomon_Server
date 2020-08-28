2ed60af254e95fc204f77fbe6a005335b34be816
김경훈 <kidi052812@gmail.com>
2020-08-28 15:02:27 +0900

    [ADD] : App.config Setting

---
+++ Bulletin_Server/Bulletin_Server/Interface/IService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulletin_Server
+{
+    [ServiceContract]
+    public interface IService
+    {
+
+    }
+
+    public class Response<T>
+    {
+        public string message { get; set; }
+        public HttpStatusCode status { get; set; }
+        public T data { get; set; }
+    }
+}
