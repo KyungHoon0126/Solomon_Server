511e4301eea648eeb2829b0f89fa2cc50dae67fd
김경훈 <kidi052812@gmail.com>
2020-08-29 19:50:02 +0900

    [ADD] : DBManager, ModelBase

--- Bulletin_Server/Bulletin_Server/DataBase/DBManager.cs
+++ Bulletin_Server/Bulletin_Server/DataBase/DBManager.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bulletin_Server.DataBase
 {
-    public class DBManager
+    public class DBManager<T>
     {
+        public static List<T> GetList(IDbConnection conn, IDbTransaction tran, string search)
+        {
+            string sql = @"";
+            return SqlMapper.Query<T>(conn, sql, new { search = search }).ToList();
+        }
 
+        public int Insert(IDbConnection conn , IDbTransaction tran = null)
+        {
+            return SqlMapper.Execute(conn, "insert query", this, tran);
+        }
+
+        public int Update(IDbConnection conn, IDbTransaction tran = null)
+        {
+            return SqlMapper.Execute(conn, "update query", this, tran);
+        }
+
+        public int Delete(IDbConnection conn, IDbTransaction tran = null)
+        {
+            return SqlMapper.Execute(conn, "delete query", this, tran);
+        }
     }
 }
---
+++ Bulletin_Server/Bulletin_Server/DataBase/ModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulletin_Server.DataBase
+{
+    public abstract class ModelBase
+    {
+        public enum ItemStateEnum
+        {
+            None = 0,
+            Modified = 1,
+            Added = 2,
+            Deleted = 3
+        }
+
+        public ItemStateEnum ItemState { get; set; } = ItemStateEnum.None;
+    }
+}
