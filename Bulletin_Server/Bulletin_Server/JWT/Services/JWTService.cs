7639608f951f1c853faa3ae439faa2f743945bf3
김경훈 <kidi052812@gmail.com>
2020-08-30 15:45:33 +0900

    [ADD} : GetJWTContainerModel & CreateModelToken Methods

--- Bulletin_Server/Bulletin_Server/JWT/Services/JWTService.cs
+++ Bulletin_Server/Bulletin_Server/JWT/Services/JWTService.cs
@@ -3,6 +3,7 @@ using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 
 namespace Bulletin_Server.JWT.Services
@@ -97,6 +98,40 @@ namespace Bulletin_Server.JWT.Services
             catch (Exception ex)
             {
                 throw ex;
+            }
+        }
+
+        private static JWTContainerModel GetJWTContainerModel(string name, string email)
+        {
+            return new JWTContainerModel()
+            {
+                Claims = new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, name),
+                    new Claim(ClaimTypes.Email, email)
+                }
+            };
+        }
+
+        public void CreateModelToken()
+        {
+            string name = "";
+            string email = "";
+
+            IAuthContainerModel model = GetJWTContainerModel(name, email);
+            IAuthService authService = new JWTService(model.SecretKey);
+
+            string token = authService.GenerateToken(model);
+
+            if(!authService.IsTokenValid(token))
+            {
+                throw new UnauthorizedAccessException();
+            }
+            else
+            {
+                List<Claim> claims = authService.GetTokenClaims(token).ToList();
+                Console.WriteLine(claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Name)).Value);
+                Console.WriteLine(claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Email)).Value);
             }
         }
     }
