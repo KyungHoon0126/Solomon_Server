18935c3ae90598a96bb9fb439b37fef6cfeda83e
김경훈 <kidi052812@gmail.com>
2020-09-01 14:44:07 +0900

    [ADD] : SignUp Feature & DataBase Save

--- Bulletin_Server/Bulletin_Server/Services/MemberService.cs
+++ Bulletin_Server/Bulletin_Server/Services/MemberService.cs
@@ -1,9 +1,14 @@
-﻿using Bulletin_Server.JWT.Models;
+﻿using Bulletin_Server.DataBase;
+using Bulletin_Server.JWT.Models;
 using Bulletin_Server.JWT.Services;
 using Bulletin_Server.Model.Member;
 using Bulletin_Server.Utils;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Security.Claims;
 
@@ -11,9 +16,60 @@ namespace Bulletin_Server.Service
 {
     public partial class BulletinService : IService
     {
-        public Response<User> Login(string id, string password, string email)
+        DBManager<UserModel> userDBManager = new DBManager<UserModel>();
+
+        public Response<UserModel> SignUp(string id, string pw, string name, string email)
         {
-            User user = new User();
+            if(id != null && pw != null && name != null && email != null)
+            {
+                try
+                {
+                    string connectionString = $"SERVER=localhost;DATABASE=Bulletin;UID=root;PASSWORD=#kkh03kkh#";
+                    using (IDbConnection db = new MySqlConnection(connectionString))
+                    {
+                        db.Open();
+
+                        var model = new UserModel();
+                        model.id = id;
+                        model.pw = pw;
+                        model.name = name;
+                        model.email = email;
+
+                        string insertSql = @"
+INSERT INTO member_tb (
+    id,
+    pw,
+    name,
+    email
+) 
+VALUES(
+    @id,
+    @pw,
+    @name,
+    @email
+);";
+                        userDBManager.Insert(db, insertSql, model);
+                        var resp = new Response<UserModel> { data = { }, message = "성공적으로 회원가입이 신청되었습니다.", status = ResponseStatus.OK };
+                        return resp;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return new Response<UserModel> { data = { }, message = "서버 오류", status = ResponseStatus.InternalServerError };
+                }
+            }
+            else
+            {
+                var resp = new Response<UserModel> { data = { }, message = "검증 오류", status = ResponseStatus.BadRequest };
+                return resp;
+            }
+        }
+
+        // TODO : email 빼기
+        public Response<UserModel> Login(string id, string password, string email)
+        {
+            UserModel user = new UserModel();
 
             IAuthContainerModel model = JWTService.GetJWTContainerModel(email);
             IAuthService authService = new JWTService(model.SecretKey);
@@ -29,7 +85,7 @@ namespace Bulletin_Server.Service
                 List<Claim> claims = authService.GetTokenClaims(token).ToList();
                 Console.WriteLine("Login Eamil : " + claims.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Email)).Value);
                 user.id = id;
-                user.password = password;
+                user.pw = password;
                 user.email = email;
                 user.token = token;
             }
@@ -37,13 +93,13 @@ namespace Bulletin_Server.Service
             if (token != null && token.Length > 0)
             {
                 Console.WriteLine("로그인 : " + ResponseStatus.OK);
-                var resp = new Response<User> { data = user, message = "성공적으로 처리되었습니다.", status = ResponseStatus.OK };
+                var resp = new Response<UserModel> { data = user, message = "성공적으로 처리되었습니다.", status = ResponseStatus.OK };
                 return resp;
             }
             else
             {
                 Console.WriteLine("로그인 : " + ResponseStatus.Unauthorized);
-                var resp = new Response<User> { data = user, message = "검증 오류.", status = ResponseStatus.Unauthorized };
+                var resp = new Response<UserModel> { data = user, message = "검증 오류.", status = ResponseStatus.Unauthorized };
                 return resp;
             }
         }
--- Bulletin_Server/Bulletin_Server/Utils/ResponseStatus.cs
+++ Bulletin_Server/Bulletin_Server/Utils/ResponseStatus.cs
@@ -7,5 +7,8 @@ namespace Bulletin_Server.Utils
     {
         public static int OK = Convert.ToInt32(HttpStatusCode.OK);
         public static int NotFound = Convert.ToInt32(HttpStatusCode.NotFound);
+        public static int Unauthorized = Convert.ToInt32(HttpStatusCode.Unauthorized);
+        public static int BadRequest = Convert.ToInt32(HttpStatusCode.BadRequest);
+        public static int InternalServerError = Convert.ToInt32(HttpStatusCode.InternalServerError);
     }
 }
