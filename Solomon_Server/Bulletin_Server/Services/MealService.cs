7c83d90838aeb3e988fdcb2a398e6178d842b46b
김경훈 <kidi052812@gmail.com>
2020-09-05 10:06:14 +0900

    [FIX] : 토큰 유효시에만 급식 API 조회
    로그인 후 토큰이 유효할 때만 급식 API 조회 가능하도록 변경

--- Solomon_Server/Bulletin_Server/Services/MealService.cs
+++ Solomon_Server/Bulletin_Server/Services/MealService.cs
@@ -1,9 +1,11 @@
-﻿using Bulletin_Server.Model.Meal;
+﻿using Bulletin_Server.Common;
+using Bulletin_Server.Model.Meal;
 using Bulletin_Server.Utils;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Net;
+using System.ServiceModel.Web;
 using System.Text;
 using hap = HtmlAgilityPack;
 
@@ -13,34 +15,51 @@ namespace Bulletin_Server.Service
     {
         public Response<MealInfo> GetMealData()
         {
-            WebClient webClient = new WebClient();
-            webClient.Headers["Content-Type"] = "application/json";
-            webClient.Encoding = Encoding.UTF8;
-            string html = webClient.DownloadString("https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7240393&MLSV_YMD="
-                                                   + DateTime.Now.ToString("yyyyMM") + "&type=json&KEY=9b89605504b946bfab0c06c0ceb0a69a");
-            hap.HtmlDocument document = new hap.HtmlDocument();
-            document.LoadHtml(html);
-            JObject jObject = JObject.Parse(document.Text);
-            MealInfo mealData = JsonConvert.DeserializeObject<MealInfo>(jObject.ToString());
-            
-            for (int i = 0; i < mealData.meal.Count; i++)
+            WebOperationContext webOperationContext = WebOperationContext.Current;
+            string requestHeaderValue = webOperationContext.IncomingRequest.Headers["token"].ToString();
+
+            MealInfo tempModel = new MealInfo();
+
+            if (ComDef.jwtService.IsTokenValid(requestHeaderValue) == true)
             {
-                if (mealData.meal[i].row == null)
+                WebClient webClient = new WebClient();
+                webClient.Headers["Content-Type"] = "application/json";
+                webClient.Encoding = Encoding.UTF8;
+
+                string html = webClient.DownloadString("https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7240393&MLSV_YMD="
+                                                       + DateTime.Now.ToString("yyyyMM") + "&type=json&KEY=9b89605504b946bfab0c06c0ceb0a69a");
+                hap.HtmlDocument document = new hap.HtmlDocument();
+                document.LoadHtml(html);
+
+                JObject jObject = JObject.Parse(document.Text);
+
+                MealInfo mealData = JsonConvert.DeserializeObject<MealInfo>(jObject.ToString());
+
+                for (int i = 0; i < mealData.meal.Count; i++)
                 {
-                    mealData.meal.Remove(mealData.meal[i]);
+                    if (mealData.meal[i].row == null)
+                    {
+                        mealData.meal.Remove(mealData.meal[i]);
+                    }
                 }
-            }
-            
-            if (mealData == null || mealData.meal.Count < 0)
-            {
-                Console.WriteLine("급식 : " + ResponseStatus.NotFound);
-                var resp = new Response<MealInfo> { data = mealData, message = "급식 설정이 필요합니다.", status = ResponseStatus.NotFound };
-                return resp;
+
+                if (mealData == null || mealData.meal.Count < 0)
+                {
+                    Console.WriteLine("급식 : " + ResponseStatus.NotFound);
+                    var resp = new Response<MealInfo> { data = tempModel, message = "급식 설정이 필요합니다.", status = ResponseStatus.NotFound };
+                    return resp;
+                }
+                else
+                {
+                    Console.WriteLine("급식 : " + ResponseStatus.OK);
+                    var resp = new Response<MealInfo> { data = mealData, message = "급식 조회에 성공하였습니다.", status = ResponseStatus.OK };
+                    return resp;
+                }
             }
             else
             {
-                Console.WriteLine("급식 : " + ResponseStatus.OK);
-                var resp = new Response<MealInfo> { data = mealData, message = "급식 조회에 성공하였습니다.", status = ResponseStatus.OK };
+                Console.WriteLine("급식 : " + ResponseStatus.BadRequest);
+                var resp = new Response<MealInfo> { data = tempModel, message = "토큰이 만료되었습니다.", status = ResponseStatus.BadRequest };
                 return resp;
             }
         }
