7a18629b477fec858268bdde78676fb8819e6494
김경훈 <kidi052812@gmail.com>
2020-09-18 15:33:29 +0900

    [ADD] : MemberResult, CommentsResult

---
+++ Solomon_Server/Bulletin_Server/Results/CommentResult/CommentsResult.cs
@@ -0,0 +1,10 @@
+﻿using Solomon_Server.Models.Bulletin;
+using System.Collections.Generic;
+
+namespace Solomon_Server.Results.CommentResults
+{
+    public class CommentsResult
+    {
+        public List<CommentModel> comments = new List<CommentModel>();
+    }
+}
