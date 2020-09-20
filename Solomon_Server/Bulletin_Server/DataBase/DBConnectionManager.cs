802e0116850d67118773774617a5b5e6ebbd967d
김경훈 <kidi052812@gmail.com>
2020-09-20 16:08:43 +0900

    [ADD] : DBConnectionManager, MSqlDBConnectionManager,  MySqlDBConnectionManager

---
+++ Solomon_Server/Bulletin_Server/DataBase/DBConnectionManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Solomon_Server.DataBase
+{
+    public abstract class DBConnectionManager
+    {
+        public abstract IDbConnection GetConnection();
+    }
+}
---
+++ Solomon_Server/Bulletin_Server/DataBase/MySqlDBConnectionManager.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Solomon_Server.DataBase
+{
+    // TODO : Repository Public 시, DB 정보 제외.
+    public class MySqlDBConnectionManager : DBConnectionManager
+    {
+        private readonly string DATA_BASE_URL = $"SERVER=localhost;DATABASE=Bulletin;UID=root;PASSWORD=#kkh03kkh#;allow user variables=true";
+
+        public override IDbConnection GetConnection()
+        {
+            IDbConnection db = new MySqlConnection(DATA_BASE_URL);
+            return db;
+        }
+    }
+}
