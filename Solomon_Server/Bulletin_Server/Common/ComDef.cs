c90fe8af71c2914ac6d82b559aa2e32ebe1359eb
김경훈 <kidi052812@gmail.com>
2020-09-20 10:38:46 +0900

    [ADD] : WrapAPI.cs, API Request TimeStamp

--- Solomon_Server/Bulletin_Server/Common/ComDef.cs
+++ Solomon_Server/Bulletin_Server/Common/ComDef.cs
@@ -1,17 +1,51 @@
 ﻿using Solomon_Server.JWT.Models;
 using Solomon_Server.JWT.Services;
+using Solomon_Server.Utils;
+using System;
 using System.ServiceModel.Web;
 
 namespace Solomon_Server.Common
 {
     // TODO : Repository Public 시, JWT Secret Key, Meal API Key, DB 정보 제외.
     public class ComDef
-    {
+    {   
         // Token Verification
         public static JWTContainerModel jWTContainerModel = new JWTContainerModel();
         public static JWTService jwtService = new JWTService(jWTContainerModel.SecretKey);
 
         public static readonly string DATA_BASE_URL = $"SERVER=localhost;DATABASE=Bulletin;UID=root;PASSWORD=#kkh03kkh#;allow user variables=true";
+
+        // Request Time Log
+        public static string CheckRequestTime()
+        {
+            DateTime time = DateTime.Now;
+            int year = time.Year;
+            int month = time.Month;
+            int day = time.Day;
+            int hour = time.Hour;
+            int minute = time.Minute;
+            int second = time.Second;
+
+            string msg = string.Empty;
+            msg += "[";    
+            msg += year.ToString() + "년 ";
+            msg += month.ToString() + "월 ";
+            msg += day.ToString() + "일 ";
+            msg += hour.ToString() + "시 ";
+            msg += minute.ToString() + "분 ";
+            msg += second.ToString() + "초 ";
+            msg += "] ";
+            return msg;
+        }
+
+        // Request Log
+        public static void ShowResponseResult(string apiName, ConTextColor preColor, int status, ConTextColor setColor)
+        {
+            Console.Write(CheckRequestTime() + $"{apiName} responded ");
+            WrapAPI.SetConsoleTextColor(preColor);
+            Console.WriteLine(status);
+            WrapAPI.SetConsoleTextColor(setColor);
+        }
 
         /// <summary>
         /// Inpection Request Header's Value
