023222f0a8ce1467ac16cc5f2a47dfa9bc94b254
김경훈 <kidi052812@gmail.com>
2020-10-11 20:35:06 +0900

    [CORRECT] : Table Variable, Method Name

--- Solomon_Server/Bulletin_Server/Common/ResponseStatus.cs
+++ Solomon_Server/Bulletin_Server/Common/ResponseStatus.cs
@@ -5,15 +5,15 @@ namespace Solomon_Server.Utils
 {
     public class ResponseStatus
     {
-        public static int OK = ConvertToIntHttpStatusCode(HttpStatusCode.OK);
-        public static int NOT_FOUND = ConvertToIntHttpStatusCode(HttpStatusCode.NotFound);
-        public static int UNAUTHORIZED = ConvertToIntHttpStatusCode(HttpStatusCode.Unauthorized);
-        public static int BAD_REQUEST = ConvertToIntHttpStatusCode(HttpStatusCode.BadRequest);
-        public static int INTERNAL_SERVER_ERROR = ConvertToIntHttpStatusCode(HttpStatusCode.InternalServerError);
-        public static int CONFLICT = ConvertToIntHttpStatusCode(HttpStatusCode.Conflict);
-        public static int CREATED = ConvertToIntHttpStatusCode(HttpStatusCode.Created);
+        public static int OK = ConvertHttpStatusCodeToInt(HttpStatusCode.OK);
+        public static int NOT_FOUND = ConvertHttpStatusCodeToInt(HttpStatusCode.NotFound);
+        public static int UNAUTHORIZED = ConvertHttpStatusCodeToInt(HttpStatusCode.Unauthorized);
+        public static int BAD_REQUEST = ConvertHttpStatusCodeToInt(HttpStatusCode.BadRequest);
+        public static int INTERNAL_SERVER_ERROR = ConvertHttpStatusCodeToInt(HttpStatusCode.InternalServerError);
+        public static int CONFLICT = ConvertHttpStatusCodeToInt(HttpStatusCode.Conflict);
+        public static int CREATED = ConvertHttpStatusCodeToInt(HttpStatusCode.Created);
 
-        public static int ConvertToIntHttpStatusCode(HttpStatusCode httpStatusCode)
+        public static int ConvertHttpStatusCodeToInt(HttpStatusCode httpStatusCode)
         {
             return Convert.ToInt32(httpStatusCode);
         }
