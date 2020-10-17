fb167b4c59110ba3c19f09d321f315baab9ff066
김경훈 <kidi052812@gmail.com>
2020-10-17 20:00:39 +0900

    [ADD] : ComUtil.cs

---
+++ Solomon_Server/Bulletin_Server/Common/ComUtil.cs
@@ -0,0 +1,55 @@
+﻿namespace Solomon_Server.Common
+{
+    public class ComUtil
+    {
+        public static int[] GetStringLengths(string item1, string item2)
+        {
+            int[] stringLengths = new int[2];
+            stringLengths[0] = item1.Trim().Length;
+            stringLengths[1] = item1.Trim().Length;
+            return stringLengths;
+        }
+
+        public static int[] GetStringLengths(string item1, string item2, string item3)
+        {
+            int[] stringLengths = new int[3];
+            stringLengths[0] = item1.Trim().Length;
+            stringLengths[1] = item1.Trim().Length;
+            stringLengths[2] = item1.Trim().Length;
+            return stringLengths;
+        }
+
+        public static int[] GetStringLengths(string item1, string item2, string item3, string item4)
+        {
+            int[] stringLengths = new int[4];
+            stringLengths[0] = item1.Trim().Length;
+            stringLengths[1] = item1.Trim().Length;
+            stringLengths[2] = item1.Trim().Length;
+            stringLengths[3] = item1.Trim().Length;
+            return stringLengths;
+        }
+
+        public static int[] GetStringLengths(string item1, string item2, string item3, string item4, string item5)
+        {
+            int[] stringLengths = new int[5];
+            stringLengths[0] = item1.Trim().Length;
+            stringLengths[1] = item1.Trim().Length;
+            stringLengths[2] = item1.Trim().Length;
+            stringLengths[3] = item1.Trim().Length;
+            stringLengths[4] = item1.Trim().Length;
+            return stringLengths;
+        }
+
+        public static int[] GetStringLengths(string item1, string item2, string item3, string item4, string item5, string item6)
+        {
+            int[] stringLengths = new int[6];
+            stringLengths[0] = item1.Trim().Length;
+            stringLengths[1] = item1.Trim().Length;
+            stringLengths[2] = item1.Trim().Length;
+            stringLengths[3] = item1.Trim().Length;
+            stringLengths[4] = item1.Trim().Length;
+            stringLengths[5] = item1.Trim().Length;
+            return stringLengths;
+        }
+    }
+}
